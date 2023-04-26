482361046da375dc686e49000d172b7d16d4cee7
christian-iuil <okoriec01@gmail.com>
2023-04-26 10:47:18 +0100

    Initial commit

---
+++ lib/contants.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+const kPrimaryColor=Color(0xff918ae2);
+const kWhiteColor=Colors.white;
+const kButtonColor=Color(0xff5d55b4);
+final kBorderRadius=BorderRadius.circular(10);
+const kImageUrl='assets/';
+const kPadding=EdgeInsets.symmetric(horizontal: 20,vertical: 30);
+SizedBox sizedHeight( double height) => SizedBox(height: height);
---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:foodui/contants.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: kPrimaryColor,
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Container(
+              padding: kPadding,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Image.asset(
+                        '${kImageUrl}welcome.png',
+                        height: 300,
+                        fit: BoxFit.fitHeight,
+
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Delicious \nfoods.',
+                          style: TextStyle(
+                              fontSize: 58,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: 2.0,
+                              color: kWhiteColor),
+                        ),
+                        sizedHeight(10),
+                        const Text(
+                          'Let us help you discover the \nbest food of the week.',
+                          style: TextStyle(
+                              fontSize: 14,
+                              height: 1.6,
+                              fontWeight: FontWeight.w400,
+                              letterSpacing: 1.5,
+                              color: kWhiteColor),
+                        ),
+                        sizedHeight(20),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                              fixedSize:
+                                  MaterialStateProperty.all(const Size(170, 60)),
+                              backgroundColor:
+                                  MaterialStateProperty.all(kButtonColor),
+                              shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                                  borderRadius: kBorderRadius))),
+                          onPressed: () {},
+                          child: Text('Get Started'),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
